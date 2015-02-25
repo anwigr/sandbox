86ef834b11250b6431a41479fc751a09339f3bfb
Andreas Wikström Grönlund <andreas.w.gronlund@gmail.com>
2015-02-25 20:43:47 +0100

    just testing

--- SandboxJava8/src/se/eforus/sandbox/SandboxJava8.java
+++ SandboxJava8/src/se/eforus/sandbox/SandboxJava8.java
@@ -5,17 +5,25 @@
  */
 package se.eforus.sandbox;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author andreaswikstromgronlund
  */
 public class SandboxJava8 {
+    
+    public void dateAndTimeAPI() {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SandboxJava8 sandboxJava8 = new SandboxJava8();
+        sandboxJava8.dateAndTimeAPI();
     }
     
 }
